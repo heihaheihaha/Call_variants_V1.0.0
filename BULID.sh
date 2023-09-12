c965694d513cec727f889b321591492bc134ba03
WANG TIAN YANG <96468382+heihaheihaha@users.noreply.github.com>
2023-09-12 17:15:15 +0800

    Update BULID.sh

--- BULID.sh
+++ BULID.sh
@@ -3,11 +3,11 @@ cd ./bin
 wget https://github.com/broadinstitute/gatk/releases/download/4.4.0.0/gatk-4.4.0.0.zip
 unzip gatk-4.4.0.0.zip
 rm -rf gatk-4.4.0.0.zip
-
+cd
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 git clone -b raredisease --single-branch https://github.com/nf-core/test-datasets.git
-
-mamba create -c conda-forge -c bioconda -n snakemake snakemake
+# mamba create -c conda-forge -c bioconda -n snakemake snakemake
+# mamba env create -f first_
