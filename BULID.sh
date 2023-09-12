b541de99bd4aab8afc39cb871a07e91b6fdd7511
WANG TIAN YANG <96468382+heihaheihaha@users.noreply.github.com>
2023-09-12 17:17:37 +0800

    Update BULID.sh

--- BULID.sh
+++ BULID.sh
@@ -10,4 +10,4 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 git clone -b raredisease --single-branch https://github.com/nf-core/test-datasets.git
 # mamba create -c conda-forge -c bioconda -n snakemake snakemake
-# mamba env create -f first_
+# mamba env create -f first_step_mamba.yml
